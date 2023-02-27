42f8d22501617ead2e35b12546ae43b7a7da4fc9
Raphael Bertholucci <raphaelbertholucci@gmail.com>
2023-02-27 20:05:37 -0300

    splash screen added

--- features/home/src/main/kotlin/com/bertholucci/home/ui/home/HomeActivity.kt
+++ features/home/src/main/kotlin/com/bertholucci/home/ui/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.bertholucci.home.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.bertholucci.home.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class HomeActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
